e7c31ede806d7cf8af311fadfe5841be90dbbd52
yuusoyeon <yuusoyean123@naver.com>
2023-11-22 16:58:29 +0900

    C언어 프로젝트

--- sources/StudyPlan_30.c
+++ sources/StudyPlan_30.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#define MAX_SUBJECTS 10
+#define MAX_DAYS 30
+
 int main() {
     int subjectCount, totalDays;
 
@@ -7,26 +10,39 @@ int main() {
     printf("시험을 보는 과목의 개수를 입력하세요: ");
     scanf_s("%d", &subjectCount);
 
+    // 과목명과 시험 날짜를 저장하는 배열
+    char subjects[MAX_SUBJECTS][50];
+    char examDates[MAX_SUBJECTS][20];
+
+    // 각 과목의 이름과 시험 날짜 입력
+    for (int i = 0; i < subjectCount; ++i) {
+        printf("과목 %d의 이름을 입력하세요: ", i + 1);
+        scanf_s("%s", subjects[i], sizeof(subjects[i]));
+
+        printf("과목 %d의 시험 날짜를 입력하세요: ", i + 1);
+        scanf_s("%s", examDates[i], sizeof(examDates[i]));
+    }
+
     // 전체 기간 계산 (한 회독에 필요한 일 수로 나누어 전체 일 수를 구함)
     totalDays = subjectCount * 3 * subjectCount;
 
     printf("\n[Study Plan]\n");
 
     // 로테이션 계획 출력
-    for (int day = 1; day <= 30 && day <= totalDays; ++day) {
-        char subject = 'A' + ((day - 1) / 3) % subjectCount;
+    for (int day = 1; day <= MAX_DAYS && day <= totalDays; ++day) {
+        int subjectIndex = ((day - 1) / 3) % subjectCount;
 
-        printf("Day %d: ", day);
+        printf("Day %d: %s ", day, subjects[subjectIndex]);
 
         switch ((day - 1) % 3) {
         case 0:
-            printf("%c %d회독\n", subject, ((day - 1) / (3 * subjectCount)) + 1);
+            printf("%d회독\n", ((day - 1) / (3 * subjectCount)) + 1);
             break;
         case 1:
-            printf("%c 문제풀이&오답 체크\n", subject);
+            printf("문제풀이&오답 체크\n");
             break;
         case 2:
-            printf("%c 백지 테스트\n", subject);
+            printf("백지 테스트\n");
             break;
         }
 
