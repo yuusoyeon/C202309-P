3462f8774a733b5e2a5cab0eb21bade1c3225fc9
yuusoyeon <yuusoyean123@naver.com>
2023-11-24 03:28:49 +0900

    C언어 프로젝트

--- sources/StudyPlan_30.c
+++ sources/StudyPlan_30.c
@@ -4,6 +4,16 @@
 #define MAX_DAYS 30
 #define MAX_CONCEPTS 10
 
+// 메뉴 출력 함수
+void printMenu() {
+    printf("\n[메뉴]\n");
+    printf("1. 30일용 계획 세우기\n");
+    printf("2. 부족 개념 입력\n");
+    printf("3. 수행 여부 입력하기\n");
+    printf("4. 종료\n");
+    printf("메뉴를 선택하세요: ");
+}
+
 int main() {
     int subjectCount, totalDays;
 
@@ -44,43 +54,69 @@ int main() {
         }
     }
 
-    // 전체 기간 계산 (한 회독에 필요한 일 수로 나누어 전체 일 수를 구함)
-    totalDays = subjectCount * 3 * subjectCount;
+    // 메뉴 선택 및 처리
+    int menuChoice;
+    do {
+        printMenu();
+        scanf_s("%d", &menuChoice);
 
-    printf("\n[Study Plan]\n");
-
-    // 로테이션 계획 및 부족한 개념 출력
-    for (int day = 1; day <= MAX_DAYS && day <= totalDays; ++day) {
-        int subjectIndex = ((day - 1) / 3) % subjectCount;
-
-        printf("Day %d: %s ", day, subjects[subjectIndex]);
-
-        switch ((day - 1) % 3) {
-        case 0:
-            printf("%d회독\n", ((day - 1) / (3 * subjectCount)) + 1);
-            break;
+        switch (menuChoice) {
         case 1:
-            printf("문제풀이&오답 체크\n");
+            // 30일용 계획 세우기
+            totalDays = subjectCount * 3 * subjectCount;
+            printf("\n[스터디 계획]\n");
+
+            // 로테이션 계획 및 부족한 개념 출력
+            for (int day = 1; day <= MAX_DAYS && day <= totalDays; ++day) {
+                int subjectIndex = ((day - 1) / 3) % subjectCount;
+
+                printf("Day %d: %s ", day, subjects[subjectIndex]);
+
+                switch ((day - 1) % 3) {
+                case 0:
+                    printf("%d회독\n", ((day - 1) / (3 * subjectCount)) + 1);
+                    break;
+                case 1:
+                    printf("문제풀이&오답 체크\n");
+                    break;
+                case 2:
+                    printf("백지 테스트\n");
+                    break;
+                }
+
+                // 부족한 개념 출력
+                printf("부족한 개념: ");
+                for (int i = 0; i < MAX_CONCEPTS; ++i) {
+                    // 'q' 입력이면 출력 중단
+                    if (concepts[subjectIndex][i][0] == 'q') {
+                        break;
+                    }
+                    printf("%s - %s ", concepts[subjectIndex][i], definitions[subjectIndex][i]);
+                }
+
+                if ((day % (3 * subjectCount)) == 0 && day != totalDays) {
+                    printf("\n");
+                }
+            }
             break;
         case 2:
-            printf("백지 테스트\n");
+            // 부족 개념 입력
+            // TODO: 구현 필요
             break;
+        case 3:
+            // 수행 여부 입력하기
+            // TODO: 구현 필요
+            break;
+        case 4:
+            // 종료
+            printf("프로그램을 종료합니다.\n");
+            break;
+        default:
+            printf("잘못된 메뉴 선택입니다. 다시 선택하세요.\n");
         }
 
-        // 부족한 개념 출력
-        printf("부족한 개념: ");
-        for (int i = 0; i < MAX_CONCEPTS; ++i) {
-            // 'q' 입력이면 출력 중단
-            if (concepts[subjectIndex][i][0] == 'q') {
-                break;
-            }
-            printf("%s - %s ", concepts[subjectIndex][i], definitions[subjectIndex][i]);
-        }
-
-        if ((day % (3 * subjectCount)) == 0 && day != totalDays) {
-            printf("\n");
-        }
-    }
+    } while (menuChoice != 4);
 
     return 0;
 }
+
